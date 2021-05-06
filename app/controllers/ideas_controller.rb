5eda4dce589b3eb5ac863fa118b782a8c30b1778
caleb.faulhaber@gmail.com <caleb.faulhaber@gmail.com>
2021-05-06 15:23:48 -0700

    2 valid tests need to be fixed still. added navbar

--- app/controllers/ideas_controller.rb
+++ app/controllers/ideas_controller.rb
@@ -1,31 +1,56 @@
 class IdeasController < ApplicationController
 
+    before_action :find_idea, only: [:show, :edit, :update, :destroy]
+
     def new
         @idea = Idea.new
     end
 
-    def show
+    def create
+        @idea = Idea.new idea_params
+        
+        if @idea.save
+            flash[:notice] = "Idea created successfully"
+            redirect_to ideas_path
+        else
+            render :new
+        end
+    end
 
+    def show
+        # @idea = Idea.
     end
 
     def index
-
+        @ideas = Idea.all.order(created_at: :desc)
     end
 
-    def edit
+    # def edit
 
-    end
+    # end
 
-    def create
-        
+    def update
+        if @idea.update idea_params
+            redirect_to idea_path(@idea)
+        else
+            render :edit
+        end
     end
 
     def destroy
-
+        @idea.destroy
+        redirect_to ideas_path
     end
 
 
     private
 
+    def idea_params
+        params.require(:idea).permit(:title, :body)
+    end
+
+    def find_idea
+        @idea = Idea.find params[:id]
+    end
 
 end
--- app/models/idea.rb
+++ app/models/idea.rb
@@ -1,4 +1,12 @@
 class Idea < ApplicationRecord
     validates :title, presence: true, uniqueness: true
 
+    before_save :capitalize_title
+
+    private
+
+    def capitalize_title
+        self.title.capitalize!
+    end
+
 end
